72f35f863bbd5a08c3438f37105d6be259b41e08
Paul Guillard <paul@benuvillageesch.lu>
2022-05-16 15:07:25 +0200

    Local fonts in CSS sheet to avoid loading issues.

--- resources/views/layouts/html_general_layout.blade.php
+++ resources/views/layouts/html_general_layout.blade.php
@@ -27,11 +27,9 @@
         <meta name="theme-color" content="#ffffff">
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<!--         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
 
         <!-- Favicon -->
 
