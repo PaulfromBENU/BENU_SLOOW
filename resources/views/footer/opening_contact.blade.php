2e5339a0823b8594c200b8192a3699b0c4d319ed
Paul Guillard <paul@benuvillageesch.lu>
2022-05-20 14:46:02 +0200

    Added Google map iframe below the contact section.

--- resources/views/footer/opening_contact.blade.php
+++ resources/views/footer/opening_contact.blade.php
@@ -22,4 +22,11 @@
 	<div class="footer-contact__form m-auto" id="contact-form">
 		@livewire('services.contact-form')
 	</div>
+
+	<div class="footer-contact__map">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1295.8629770389205!2d5.9767905045451695!3d49.489683434955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe615dade8d8f1dbc!2zNDnCsDI5JzIyLjkiTiA1wrA1OCczOS43IkU!5e0!3m2!1sen!2slu!4v1653050227766!5m2!1sen!2slu" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+	<p class="text-center w-3/4 m-auto pt-5 pb-5">
+		{{ __('welcome.map-explanation') }}
+	</p>
 </section>
