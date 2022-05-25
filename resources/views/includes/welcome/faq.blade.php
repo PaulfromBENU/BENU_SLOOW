d54a93a48fcb6ca7195241d76de8e4f3ab02a7a4
Paul Guillard <paul@benuvillageesch.lu>
2022-05-25 16:47:01 +0200

    Updated FAQ structure and content. Updated translations.

--- resources/views/includes/welcome/faq.blade.php
+++ resources/views/includes/welcome/faq.blade.php
@@ -2,40 +2,117 @@
 	<div class="welcome-faq__container pb-10">
 		<h2 class="welcome-faq__title primary-color" id="faq-anchor">{!! __('services.faq-title') !!}</h2>
 		<ul class="welcome-faq__accordion">
-			@for($index = 1; $index <= $faq_titles_count; $index ++)
-				<li>
-					<div class="welcome-faq__accordion__header flex justify-between">
-						<p class="w-4/5">{!! __('services.faq-group-title-'.$index) !!}</p>
-						<div class="w-1/6 flex flex-col justify-center">
-							<p class="flex justify-end">
-								<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
-							</p>
-						</div>
-					</div>
-
-					<div class="welcome-faq__accordion__answer" style="display: none;">
-						<p class="welcome-faq__accordion__answer__txt">
-							{!! __('services.faq-group-desc-'.$index) !!}
+			<li>
+				<div class="welcome-faq__accordion__header flex justify-between">
+					<p class="w-4/5">{!! __('services.faq-group-title-1') !!}</p>
+					<div class="w-1/6 flex flex-col justify-center">
+						<p class="flex justify-end">
+							<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
 						</p>
-						<ul class="mb-10">
-							@for($sub_index = 1; $sub_index <= $faq_subtitles_count[$index]; $sub_index ++)
-								<li>
-									<div class="flex justify-between welcome-faq__accordion__answer__header">
-										<p>{!! __('services.faq-group-'.$index.'-question-title-'.$sub_index) !!}</p>
-										<p>
-											<span class="welcome-faq__accordion__answer__header__plus">+</span>
-											<span class="welcome-faq__accordion__answer__header__minus" style="display: none;">-</span>
-										</p>
-									</div>
-									<p class="welcome-faq__accordion__answer__subanswer" style="display: none;">
-										{!! __('services.faq-group-'.$index.'-question-content-'.$sub_index) !!}
-									</p>
-								</li>
-							@endfor
-						</ul>
 					</div>
-				</li>
-			@endfor
+				</div>
+
+				<div class="welcome-faq__accordion__answer" style="display: none;">
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-1-1') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-1-2') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-1-3') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-1-4') !!}
+					</p>
+				</div>
+			</li>
+
+			<li>
+				<div class="welcome-faq__accordion__header flex justify-between">
+					<p class="w-4/5">{!! __('services.faq-group-title-2') !!}</p>
+					<div class="w-1/6 flex flex-col justify-center">
+						<p class="flex justify-end">
+							<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
+						</p>
+					</div>
+				</div>
+
+				<div class="welcome-faq__accordion__answer" style="display: none;">
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-2-1') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-2-2') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-2-3') !!}
+					</p>
+				</div>
+			</li>
+
+			<li>
+				<div class="welcome-faq__accordion__header flex justify-between">
+					<p class="w-4/5">{!! __('services.faq-group-title-3') !!}</p>
+					<div class="w-1/6 flex flex-col justify-center">
+						<p class="flex justify-end">
+							<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
+						</p>
+					</div>
+				</div>
+
+				<div class="welcome-faq__accordion__answer" style="display: none;">
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-3-1') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-3-2') !!}
+					</p>
+				</div>
+			</li>
+
+			<li>
+				<div class="welcome-faq__accordion__header flex justify-between">
+					<p class="w-4/5">{!! __('services.faq-group-title-4') !!}</p>
+					<div class="w-1/6 flex flex-col justify-center">
+						<p class="flex justify-end">
+							<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
+						</p>
+					</div>
+				</div>
+
+				<div class="welcome-faq__accordion__answer" style="display: none;">
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-4-1') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-4-2') !!}
+					</p>
+				</div>
+			</li>
+
+			<li>
+				<div class="welcome-faq__accordion__header flex justify-between">
+					<p class="w-4/5">{!! __('services.faq-group-title-5') !!}</p>
+					<div class="w-1/6 flex flex-col justify-center">
+						<p class="flex justify-end">
+							<img src="{{ asset('images/pictures/chevron_bottom_white.png') }}" class="welcome-faq__accordion__header__chevron">
+						</p>
+					</div>
+				</div>
+
+				<div class="welcome-faq__accordion__answer" style="display: none;">
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-5-1') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-5-2') !!}
+					</p>
+					<p class="welcome-faq__accordion__answer__txt">
+						{!! __('services.faq-group-desc-5-3') !!}
+					</p>
+				</div>
+			</li>
 		</ul>
 	</div>
 
