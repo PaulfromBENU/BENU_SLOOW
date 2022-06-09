25a675f3e90e5778501238f969a58022a3e836b5
Paul Guillard <paul@benuvillageesch.lu>
2022-06-09 15:20:07 +0200

    Minor display update on back-office.

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,5 +1,5 @@
 <!-- <img src="{{ asset('/images/pictures/logo.svg') }}" alt="Logo" class="h-10"> -->
-<div class="flex">
+<div class="flex justify-center w-full">
 	<img src="http://benu_sloow.test/images/svg/benu-icon-menu-scroll.svg" class="filament-logo" style="display: block;">
 	<h1 class="filament-logo__title">BENU SLOOW</h1>
 </div>
