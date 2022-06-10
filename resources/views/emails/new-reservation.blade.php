718f6f10a086743f388fd0770a447feb0c7eeb7b
Paul Guillard <paul@benuvillageesch.lu>
2022-06-10 10:24:44 +0200

    Improved e-mail display (font family bug fix for Outlook). Improvement on concept display with parallax.

--- resources/views/emails/new-reservation.blade.php
+++ resources/views/emails/new-reservation.blade.php
@@ -13,44 +13,54 @@
 	<title>Demande de réservation - BENU SLOOW</title>
 </head>
 <body style="width: 80%; margin-left: 10%; font-family: 'Barlow';">
+	<style type=“text/css”>
+		@import url (https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap) ;
+	</style>
+	<!--[if mso]>
+	<style type="text/css">
+	body, table, td {font-family: Barlow !important;}
+	</style>
+	<![endif]-->
 	<div style="width: 100%; margin-bottom: 50px; text-align: center;">
 		<img src="{{ $message->embed(public_path('/images/pictures/logo_benu_green.png')) }}" style="height: 180px; margin: auto;" />
 	</div>
-	<div>
-		<p>
-			<strong>{{ __('emails.new-request-hello') }} {{ $reservation->first_name }} !</strong>
-		</p>
-		<p>
-			{{ __('emails.new-request-txt-1') }}
-		</p>
-		<p>
-			{{ __('emails.new-request-txt-2') }} <a href="{{ route('home', ['locale' => app()->getLocale()]) }}">sloow.benu.lu</a>.
-		</p>
-		<!-- <p>
-			{{ __('emails.new-request-details') }}&nbsp;:
-		</p>
-		<ul>
-			<li>{{ __('emails.new-request-date') }} : {{ Carbon\Carbon::parse($reservation->opening->date)->format('d\/m\/Y') }}</li>
-			<li>{{ __('emails.new-request-time') }} : {{ $reservation->opening->starting_hour }}</li>
-			<li>{{ __('emails.new-request-number-of-seats') }} : {{ $reservation->seats }}</li>
-			<li>{{ __('emails.new-request-address') }} : {{ __('emails.new-request-restaurant-address') }}</li>
-		</ul> -->
-<!-- 		<p>
-			Le service commencera à l'heure indiquée, merci d'être le plus à l'heure possible pour ne rien rater de l'expérience !
-		</p> -->
-		<!-- <p>
-			En cas d'annulation, merci de prévenir au plus vite afin que nous puissions réorganiser le service.
-		</p>
+	<font face="'Barlow';">
+		<div>
+			<p>
+				<strong>{{ __('emails.new-request-hello') }} {{ $reservation->first_name }} !</strong>
+			</p>
+			<p>
+				{{ __('emails.new-request-txt-1') }}
+			</p>
+			<p>
+				{{ __('emails.new-request-txt-2') }} <a href="{{ route('home', ['locale' => app()->getLocale()]) }}">sloow.benu.lu</a>.
+			</p>
+			<!-- <p>
+				{{ __('emails.new-request-details') }}&nbsp;:
+			</p>
+			<ul>
+				<li>{{ __('emails.new-request-date') }} : {{ Carbon\Carbon::parse($reservation->opening->date)->format('d\/m\/Y') }}</li>
+				<li>{{ __('emails.new-request-time') }} : {{ $reservation->opening->starting_hour }}</li>
+				<li>{{ __('emails.new-request-number-of-seats') }} : {{ $reservation->seats }}</li>
+				<li>{{ __('emails.new-request-address') }} : {{ __('emails.new-request-restaurant-address') }}</li>
+			</ul> -->
+	<!-- 		<p>
+				Le service commencera à l'heure indiquée, merci d'être le plus à l'heure possible pour ne rien rater de l'expérience !
+			</p> -->
+			<!-- <p>
+				En cas d'annulation, merci de prévenir au plus vite afin que nous puissions réorganiser le service.
+			</p>
 
-		<p>
-			Pour toute question, n'hésite pas à nous contacter par téléphone au +352 27 91 19 49.
-		</p>
-		<p>
-			À très bientôt pour partager ce repas ensemble !
-		</p> -->
-		<p>
-			<em><strong>{{ __('emails.new-request-regards') }} BENU SLO0W</strong></em>
-		</p>
-	</div>
+			<p>
+				Pour toute question, n'hésite pas à nous contacter par téléphone au +352 27 91 19 49.
+			</p>
+			<p>
+				À très bientôt pour partager ce repas ensemble !
+			</p> -->
+			<p>
+				<em><strong>{{ __('emails.new-request-regards') }} BENU SLO0W</strong></em>
+			</p>
+		</div>
+	</font>
 </body>
 </html>
